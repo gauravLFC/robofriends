36444f3359c11b6bbf5319854da46e482a142c44
Gaurav Sharma <gauravlfc.93@gmail.com>
2019-11-01 22:26:15 +0530

    Updates

---
+++ precache-manifest.9d4e7ef13c122a52e9b06519c23baec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1886c4a252708484498efe2bd1a0b615",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6694b79a0557e9a0c2ff",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bcc3fcc10e95b36544d9",
+    "url": "/robofriends/static/css/main.50ce8ac7.chunk.css"
+  },
+  {
+    "revision": "6694b79a0557e9a0c2ff",
+    "url": "/robofriends/static/js/2.aa2f3230.chunk.js"
+  },
+  {
+    "revision": "bcc3fcc10e95b36544d9",
+    "url": "/robofriends/static/js/main.eb2c0e31.chunk.js"
+  },
+  {
+    "revision": "5f18cae7170eeca4b795",
+    "url": "/robofriends/static/js/runtime-main.231743e3.js"
+  }
+]);
