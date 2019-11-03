b24b79d1dc591010d55a694483954821ea274cfa
Gaurav Sharma <gauravlfc.93@gmail.com>
2019-11-03 11:44:35 +0530

    Improved accessability

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,6 +6,7 @@ const SearchBox = ({setSearchField}) => {
     return (
         <div className='pa2'>
             <input 
+            aria-label='Search Robots'
             className='pa3 ba b-green bg-lightest-blue' 
             type='search' 
             placeholder='search robots' 
